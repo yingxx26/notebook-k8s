951e474742f1985230aa955bce430fe8815fee86
yingxx26 <ll>
2021-02-03 20:55:45 +0800

    TEST-1 测试

---
+++ Chapter-9/a/buildAndPushImage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#mvn package
+
+docker build -t art.local:8281/docker/notebook-k8s/notebook-service:latest .
+
+docker push art.local:8281/docker/notebook-k8s/notebook-service:latest
